2defe83b9e5cbd107c66c72a24e4384a298bd585
Howard <howard.nolan6@gmail.com>
2025-11-10 08:01:35 -0800

    Added Chi Routing, API Stubs

--- internal/http/handlers/shorten.go
+++ internal/http/handlers/shorten.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 )
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	resp := map[string]string{"status": "ok"}
+func shortenHandler(w http.ResponseWriter, r *http.Request) {
+	resp := map[string]string{"status": "ok", "message": "shorten endpoint"}
 	w.Header().Set("Content-Type", "application/json")
 	_ = json.NewEncoder(w).Encode(resp)
 }
